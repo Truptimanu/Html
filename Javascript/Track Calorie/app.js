a7b435fd15411cf2e4bbf9c72610627bd5848e48
Trupti Manu <truptimanu@gmail.com>
2021-11-21 14:52:33 -0500

    final tacalorie commit

--- Javascript/Track Calorie/app.js
+++ Javascript/Track Calorie/app.js
@@ -25,7 +25,30 @@ const StorageCtrl = (function(){
                 items = JSON.parse(localStorage.getItem('items'));
             }
             return items;
-        }
+        },
+        updateItemStorage: function(updatedItem){
+            let items = JSON.parse(localStorage.getItem('items'));
+
+            items.forEach(function(item, index){
+             if(updatedItem.id === item.id){
+                items.splice(index,1,updatedItem);     
+             }   
+            });
+            localStorage.setItem('items',JSON.stringify(items));
+        },
+        deleteItemFromStorage: function(id){
+            let items = JSON.parse(localStorage.getItem('items'));
+
+            items.forEach(function(item, index){
+             if(id === item.id){
+                items.splice(index,1);     
+             }   
+            });
+            localStorage.setItem('items',JSON.stringify(items));   
+        },
+       clearItemsFromStorage: function(){
+           localStorage.removeItem('items');
+       } 
     }
 })();
 
@@ -348,6 +371,8 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl){
        UICtrl.updateListItem(updateItem);
        const totalCalories = ItemCtrl.getTotalCalorie(); 
        UICtrl.showTotalCalories(totalCalories);
+       //uppdate local storage
+       StorageCtrl.updateItemStorage(updateItem);
 
        UICtrl.clearEditState();
         e.preventDefault();
@@ -366,6 +391,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl){
         //UICtrl.updateListItem(updateItem);
         const totalCalories = ItemCtrl.getTotalCalorie(); 
         UICtrl.showTotalCalories(totalCalories);
+
+        //delete from local storage
+        StorageCtrl.deleteItemFromStorage(currentItem.id);
  
         UICtrl.clearEditState();
         e.preventDefault();
@@ -381,7 +409,8 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl){
        //Remove from UI
        UICtrl.clearItems();
         
-       
+       //clear from local storage
+       StorageCtrl.clearItemsFromStorage();
 
     }
 
